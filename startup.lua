0fa2909cd557dc8b7541b3bc7bc26240b78b5645
Limonchik_ <36456309+LimonchikTM@users.noreply.github.com>
2021-11-14 13:47:52 +0300

    renamed file

--- startup.lua
+++ startup.lua
@@ -3,7 +3,8 @@ local monitor = peripheral.find('monitor')
 local speaker = peripheral.find("speaker")
 local width, height = monitor.getSize()
 local space = " "
-local source = "https://github.com/LimonchikTM/LimonOS"
+local source_startup = "https://raw.githubusercontent.com/LimonchikTM/LimonOS/main/startup.lua"
+local source_main = "https://raw.githubusercontent.com/LimonchikTM/LimonOS/main/main.lua"
 local source_version = "https://raw.githubusercontent.com/LimonchikTM/LimonOS/main/version.lua"
 local text_color = colors.yellow
 local second_text_color = colors.white
@@ -32,16 +33,21 @@ local title = "Limonchik's Software"
 local width, height = monitor.getSize()
 
 function autoupdate()
-    local source_status, _ = http.checkURL(source)
+    local source_status, _ = http.checkURL(source_main)
     print("Source check ... "..tostring(source_status))
     if source_status == false then
         printError("Error source check!")
     end
-    local second_source_status, _ = http.checkURL(source_version)
+    local second_source_status, _ = http.checkURL(source_main)
     if second_source_status == false then
         printError("Error second source check!")
     end
     print("Second source check ... "..tostring(second_source_status))
+    local third_source_status, _ = http.checkURL(source_version)
+    if third_source_status == false then
+        printError("Error third source check!")
+    end
+    print("Third source check ... "..tostring(third_source_status))
     sleep(1)
     local httpResponce_version = http.get(source_version)
     local allText_version = httpResponce_version.readAll()
@@ -52,13 +58,41 @@ function autoupdate()
     local version_check_status = "Error"
     if version < source_v then
         version_check_status = "Outdated version"
+        load_autoupdate_files()
     elseif version >= source_v then
         version_check_status = "Latest version"
     end
     print("Version check ... "..version_check_status)
-    local httpResponce = http.get(source)
-    local allText = httpResponce.readAll()
-    httpResponce.close()
+end
+
+function load_autoupdate_files()
+    local file_startup = "startup.lua"
+    local fs_startup = fs.open(file_startup, "w")
+    local httpResponce_startup = http.get(source_startup)
+    local allText_startup = httpResponce_startup.readAll()
+    fs_startup.write(allText_startup)
+    fs_startup.close()
+    httpResponce_startup.close()
+
+    local file_main = "main.lua"
+    local fs_main = fs.open(file_main, "w")
+    local httpResponce_main = http.get(source_main)
+    local allText_main = httpResponce_main.readAll()
+    fs_main.write(allText_main)
+    fs_main.close()
+    httpResponce_main.close()
+
+    local file_version = "version.lua"
+    local fs_version = fs.open(file_version, "w")
+    local httpResponce_version = http.get(source_version)
+    local allText_version = httpResponce_version.readAll()
+    fs_version.write(allText_version)
+    fs_version.close()
+    httpResponce_version.close()
+    
+    print("New version downloaded!")
+    print("Rebooting to apply changes...")
+    os.reboot()
 end
 
 monitor.setTextColor(text_color)
